8e4e828378967166c7bbd28d1f5f0aa3ca6addb8
Jean-michel CERVONI <jcervoni@student.42.fr>
2022-05-24 12:12:30 +0200

    ADD MS_ECHO.C / MS_BUILTIN_PARSER.C / M_REDIRECTION.C :
    ADD ms_echo.c : implementation of builtin echo wwith "-n" opt;
    ADD ms_builtin_parser.c : WIP but workin, need to add other builtin call;
    ADD ms_redirecton.c : WIP : management of redirection in/out with dup/dup2,
    not worrking for now;

---
+++ sources/builtins/ms_builtin_parser.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_builtin_parser.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/24 08:27:19 by jcervoni          #+#    #+#             */
+/*   Updated: 2022/05/24 09:30:23 by jcervoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+void	ft_builtin_parser(t_arg *arg)
+{
+	if (arg && ft_strcmp(arg->content, "echo") == 0)
+		ft_echo(arg->next);
+}
---
+++ sources/builtins/ms_echo.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_echo.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 16:51:25 by jcervoni          #+#    #+#             */
+/*   Updated: 2022/05/24 11:27:19 by jcervoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_echo_n_opt(t_arg *arg)
+{
+	int	n_opt;
+
+	n_opt = 0;
+	if (arg && ft_strcmp(arg->content, "-n") == 0)
+		n_opt = 1;
+	return (n_opt);
+}
+
+int	ft_echo_nbr_cmd(t_arg *arg)
+{
+	int	nbr_cmd;
+
+	nbr_cmd = 0;
+	while(arg && arg->token != TOKEN_PIPE)
+	{
+		if (arg->token == TOKEN_CMD)
+			nbr_cmd++;
+		arg = arg->next;
+	}
+	return (nbr_cmd);
+}
+
+void	ft_clear_echo_args(t_arg *arg)
+{
+	t_arg	*temp;
+
+	temp = arg->next;
+	if (temp && ft_strcmp(temp->content, "echo") == 0)
+	{
+		while (temp && temp->token != TOKEN_PIPE)
+		{
+			temp = temp->next;
+			// free(arg->next->content);
+			free(arg->next);
+			arg->next = temp;
+		}
+	}
+}
+
+int	ft_echo(t_arg *arg)
+{
+	char	*to_print;
+	int		nbr_cmd;
+	int		n_opt;
+	
+	n_opt = ft_echo_n_opt(arg);
+	if (n_opt == 1)
+		arg = arg->next;
+	nbr_cmd = ft_echo_nbr_cmd(arg);
+	to_print = ft_calloc(1, 1);
+	while (arg && arg->token != TOKEN_PIPE)
+	{
+		if (arg->token == TOKEN_APPENDOUT || arg->token == TOKEN_OUTFILE
+			|| arg->token == TOKEN_INFILE || arg->token == TOKEN_HEREDOC)
+			ft_redirection(arg);
+		else
+		{
+			to_print = ft_strjoin_free(to_print, arg->content);
+			nbr_cmd--;
+			if(nbr_cmd > 0)
+				to_print = ft_strjoin_free(to_print, " ");
+		}
+		arg = arg->next;
+	}
+	if (n_opt == 0)
+		to_print = ft_strjoin_free(to_print, "\n");
+	printf("%s", to_print);
+	free(to_print);
+	return (0);
+}
