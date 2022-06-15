6460fe95d69cc6e2c8b88f82615b98fcb367b26a
Jean-michel CERVONI <jcervoni@student.42.fr>
2022-06-15 13:33:46 +0200

    UPDATE MITCH_MAIN_TEST.C :
    Update main_test : main cleaned (comments & empty lines);

--- sources/mitch_main_test.c
+++ sources/mitch_main_test.c
@@ -6,7 +6,7 @@
 /*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 09:18:40 by aweaver           #+#    #+#             */
-/*   Updated: 2022/06/14 07:43:32 by jcervoni         ###   ########.fr       */
+/*   Updated: 2022/06/15 11:41:42 by jcervoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,11 @@ int	ft_test(t_arg *arg, t_env *env)
 		return (-1);
 	flags = ft_get_var_pos(arg->content, env);
 	ft_flag_char(arg->content, flags);
-	printf("flags = %s\n", flags);
 	if (dq[0] != 0)
 		ft_set_final_q_index(arg, flags, dq, env);
 	pieces = ft_lock_expand(ft_count_expand(arg, flags, env));
 	if (!pieces)
 		return (-1);
-	// printf("ici\n");
 	ft_final_string(arg, pieces, flags, env);
 	if (flags)
 		free(flags);
@@ -39,46 +37,55 @@ int	ft_test(t_arg *arg, t_env *env)
 	return (0);
 }
 
+void	ft_get_redirections(t_arg *arg)
+{
+	if (arg)
+	{
+		arg = ft_get_infile(arg);
+		arg = ft_get_heredoc(arg);
+		arg = ft_get_outfile(arg);
+		arg = ft_get_appendout(arg);
+	}
+}
+
 int	main(int ac, char *av[], char *env[])
 {
-	char	*test;
+	char	*line;
 	t_arg	*verif;
 	t_arg	*temp;
 	t_env	*env_list;
-	
-	(void)env;
+
 	(void)ac;
 	(void)av;
 	env_list = ft_env_to_list(env);
 	while (1)
 	{
-		test = readline("Test readline :");
-		if (ft_strcmp(test, "test") == 0)
+		line = readline("Test readline :");
+		if (ft_strcmp(line, "stop") == 0)
 		{
-			free(test);
+			free(line);
+			if (verif)
+				ft_clear_arg(verif);
 			exit (1);
 		}
-		
-		verif = ft_get_args(test);
-		temp = verif;
+		verif = ft_get_args(line);
 		if (verif != NULL)
 		{
 			ft_set_token(verif);
+			temp = verif;
 			while (verif != NULL)
 			{
-				verif = ft_get_infile(verif);
-				verif = ft_get_heredoc(verif);
-				verif = ft_get_outfile(verif);
-				verif = ft_get_appendout(verif);
+				ft_get_redirections(verif);
 				if (verif->token == TOKEN_CMD)
 				{
 					if (ft_test(verif, env_list) == -1)
 					{
 						printf("Missing or extra dquote\n");
+						ft_clear_arg(temp);
+						free(line);
 						exit(1);
 					}
 				}
-				// printf("final content = %s,  token = %d\n", verif->content, verif->token);
 				verif = verif->next;
 			}
 			verif = temp;
@@ -93,9 +100,8 @@ int	main(int ac, char *av[], char *env[])
 				printf("final content = %s,  token = %d\n", verif->content, verif->token);
 				verif = verif->next;
 			}
-			// if (temp != NULL)
-			// 	ft_cleararg(temp);
 		}
+		free(line);
 	}
 	return (0);
 }
